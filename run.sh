249985a62c294de2e38b75c33c8c838f3b6e7dd9
Lucas Chiesa <lucas.chiesa@contractor.kaercher.com>
2023-11-23 14:36:04 -0300

    More fixes r2r

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-mkdir -p workspace/src
-docker run -v `pwd`/r2r:/r2r --rm -it eku-humble /bin/bash
+mkdir -p r2r
+mkdir -p cache/.cargo
+docker run -v `pwd`/r2r:/r2r -v `pwd`/cache:/home/eku/ --rm -it eku-humble /bin/bash
--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cd /r2r
 git clone https://github.com/sequenceplanner/r2r.git
+cd r2r
 . /opt/ros/humble/setup.sh
 cargo build
